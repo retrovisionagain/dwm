103a2082d43cdf90da0268d3df85eb7a930607ee
retrovisionagain <pupsikmoi2010@gmail.com>
2025-11-03 12:52:56 +0500

    suka ebaniy git

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"BAT:", "acpi --battery | cut -d, -f2",	15,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
